5cc796842e42f883f87148a300173b865f1923b0
costynus <costynus@ya.ru>
2022-11-20 16:48:10 +0300

    first commit

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/Konab/go-diplom-1/loyalty-system/config"
+)
+
+func main() {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(cfg)
+}
---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type (
+	Config struct {
+		App `yaml:"app"`
+	}
+
+	App struct {
+		Name    string `env-required:"true" yaml:"name" env:"APP_NAME"`
+		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
+	}
+)
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig("./config/config.yaml", cfg)
+	if err != nil {
+		return nil, fmt.Errorf("config: error: %w", err)
+	}
+
+	err = cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
